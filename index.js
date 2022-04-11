1126e29c7477446a7e2d3a2ffc6a9fb111c54bd6
Waqas Anjum <Qanjum96@gmail.com>
2022-04-10 23:30:02 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+const driverNames = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+return driverNames
+
+}
+
+// findMatching(drivers, 'Bobby')
+// you were matching a whole name with 1 letter 
+function fuzzyMatch(drivers, name){
+    const driverNames = drivers.filter(driver => driver.charAt(0) === name.charAt(0))
+    return driverNames 
+}
+
+// fuzzyMatch(drivers, 'Susan')
+
+
+function matchName(drivers, string){
+    const nameChecker = drivers.filter(driver => driver.name === string)
+    return nameChecker
+}
+
+// matchName(drivers, 'Bobby')
